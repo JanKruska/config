6a4e6edac4e4a6f48261c74580ca6056287212ad
Jan Kruska <JanKruska@protonmail.com>
2021-10-05 20:13:15 +0200

    Added line to install JetBrainsfont with coding ligatures

--- scripts/post_install_fixes.sh
+++ scripts/post_install_fixes.sh
@@ -2,3 +2,7 @@
 
 echo -e "Create link for askpass program, such that VSCode (and possibly others) can find it" 
 sudo ln -is /usr/bin/ksshaskpass /usr/lib/ssh/ssh-askpass
+
+echo -e "Installing JetBrainsMono font with ligature support"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
+
