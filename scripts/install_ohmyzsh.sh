861c9c10eee6a70f3e4d5ca02c32147eab501f4f
Jan Kruska <JanKruska@protonmail.com>
2021-10-26 17:45:10 +0200

    Stopped script from pulling autosuggestions package

--- scripts/install_ohmyzsh.sh
+++ scripts/install_ohmyzsh.sh
@@ -5,7 +5,7 @@ sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools
 cd "${HOME}/.oh-my-zsh/custom/plugins" || exit
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
-git clone https://github.com/zsh-users/zsh-autosuggestions.git
+# git clone https://github.com/zsh-users/zsh-autosuggestions.git
 git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git
 
 echo -e "\nAgnoster theme might need additional Powerline font to render correctly in some programs"
